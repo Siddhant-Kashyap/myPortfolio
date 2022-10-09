88ee86022acaaf81d20901709e3fdc34e9eb84c4
Siddhant-Kashyap <Kashyapsiddhant01@gmail.com>
2022-10-09 17:39:25 +0530

    Services done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Intro from './components/Introduction/Intro';
 import Navbar from './components/Navbar/Navbar';
+import Services from './components/Services/Services';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Navbar/>
       <Intro/>
+      <Services/>
       </div>
    </>
   );
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,6 +3,8 @@ import './Services.css'
 import Heart from '../../../src/icons&photo/heartemoji.png'
 import Humble from '../../../src/icons&photo/humble.png'
 import Glasses from '../../../src/icons&photo/glasses.png'
+import Card from '../card/Card'
+import Resume from '../../icons&photo/Siddhant Kashyap Resume.pdf'
 
 const Services = () => {
   return (
@@ -12,11 +14,39 @@ const Services = () => {
     <div className='awesome'>
         <span>My Awesome</span>
         <span>Services</span>
+        <span>I provide services related to Wev Development <br/>Mobile App development,MERN Stack 
+    
+        </span>
+        <a href={Resume}><button className='sButton'>Download  CV</button></a>
+        
+        <div className='blur s-blur' style={{background:"#ABF1FF94"}}></div>
 
     </div>
     {/* right side division */}
-    <div className='cards'></div>
-
+    <div className='cards'>
+      <div style={{left:'12rem'}}>
+        <Card
+        emoji ={Heart}
+        heading={'APP'}
+        detail={"AndroidStudio,ReactNative"}
+        />
+      </div>
+      {/* secondcard */}
+      <div style={{top:'12rem',left:'-4rem'}}>
+        <Card
+        emoji ={Glasses}
+        heading={'WEB'}
+        detail={"HTML,CSS,JavaScript"}
+        />
+      </div>
+      <div style={{top:'20rem',left:'20rem'}}>
+        <Card
+        emoji ={Humble}
+        heading={'TECH'}
+        detail={"MERN"}
+        />
+      </div>
+      </div>
     </div>
   )
 }
